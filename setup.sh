d6756ecf24a85299d5e03193515a960ee41a3220
An Hoang <anhoang31415@gmail.com>
2024-10-19 18:51:25 +0200

    feat(setup): add p7zip installation and update Alacritty icon process

--- setup.sh
+++ setup.sh
@@ -50,6 +50,22 @@ echo "Install mise packages..."
 echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells > /dev/null
 chsh -s /opt/homebrew/bin/fish
 
+# update alacritty icon
+update_alacritty_icon() {
+    icon_path=/Applications/Alacritty.app/Contents/Resources/alacritty.icns
+    if [ ! -f "$icon_path" ]; then
+        echo "Can't find existing icon, make sure Alacritty is installed"
+        exit 1
+    fi
+    echo "Backing up existing icon"
+    hash="$(shasum $icon_path | head -c 10)"
+    mv "$icon_path" "$icon_path.backup-$hash"
+    echo "Downloading replacement icon"
+    icon_url=https://github.com/hmarr/dotfiles/files/8549877/alacritty.icns.gz
+    curl -sL $icon_url | gunzip > "$icon_path"
+    touch /Applications/Alacritty.app
+}
+update_alacritty_icon
 
 ###############################################################################
 # MacOS Setting                                                               #
