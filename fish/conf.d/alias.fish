1c83c76112cc8d6e68d38f977424778fd1a90b03
An Hoang <anhoang31415@gmail.com>
2024-10-22 09:06:03 +0200

    feat: update command aliases in fish shell configuration

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -1,21 +1,22 @@
 # shorten long commands
-abbr kc "kubectl"
-abbr lg "lazygit"
-abbr yy "yazi"
+abbr kc kubectl
+abbr lg lazygit
+abbr yy yazi
 
 # new commands
-abbr ls "eza"
-abbr cat "bat"
-abbr benchmark "hyperfine"
-abbr exa "eza"
-abbr vim "nvim"
-abbr lf "yazi"
-abbr ranger "yazi"
-abbr jq "yq"
+abbr ls eza
+abbr cat bat
+abbr benchmark hyperfine
+abbr exa eza
+abbr vim nvim
+abbr lf yazi
+abbr ranger yazi
+abbr jq yq
 abbr shred "rm -P"
 abbr view "nvim -R"
 abbr tree "eza --tree"
 abbr help "aichat -r shell:zsh"
+abbr neofetch fastfetch
 
 # Colorize commands when possible.
 alias ls="ls -G"
