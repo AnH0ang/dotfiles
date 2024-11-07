2ec7b28d647392f002dff69974105f516acab377
An Hoang <anhoang31415@gmail.com>
2024-11-07 14:24:05 +0100

    feat: enhance ruff_format script to handle percentage-prefixed lines

--- raycast/scripts/ruff_format.sh
+++ raycast/scripts/ruff_format.sh
@@ -2,11 +2,25 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Ruff Format
+# @raycast.title Ruff Format Clipboard
 # @raycast.mode fullOutput
 
 # Optional parameters:
 # @raycast.icon 👔
 # @raycast.packageName Writing
 
-pbpaste | ~/.local/bin/ruff format - | tee | pbcopy
+# Extract the text from the clipboard, remove any leading '%' characters, and format the text
+percent_lines=$(pbpaste | grep '^%')
+formatted_out=$(pbpaste | grep -v '^%' | ~/.local/bin/ruff format -)
+
+echo -e "Formatted text copied to clipboard:"
+echo -e "-----------------------------------\n"
+
+# Output the formatted text
+if [ -n "$percent_lines" ]; then
+    echo -e "$percent_lines\n$formatted_out" | pbcopy
+    echo -e "$percent_lines\n$formatted_out"
+else
+    echo "$formatted_out" | pbcopy
+    echo "$formatted_out"
+fi
