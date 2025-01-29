f1ceaac9674bb0fc93ef412ed2cc7063603925aa
An Hoang <anhoang31415@gmail.com>
2025-01-29 16:32:26 +0100

    chore: simplify fzf configuration and update starship prompt symbols

--- fish/conf.d/profile.fish
+++ fish/conf.d/profile.fish
@@ -24,12 +24,6 @@ set -x INPUTRC "$XDG_CONFIG_HOME/readline/.inputrc"
 
 # fzf
 set -x FZF_DEFAULT_COMMAND "fd --type f --hidden --exclude .git --max-depth 4"
-set -Ux FZF_DEFAULT_OPTS "\
-    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-    --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-    --color=selected-bg:#45475a \
-    --multi"
 
 # starship
 set -x STARSHIP_CONFIG "$XDG_CONFIG_HOME/starship/starship.toml"
