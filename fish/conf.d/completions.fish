5d92c4593544a151cf7740f29a35f50b74dcedf6
An Hoang <anhoang31415@gmail.com>
2025-02-08 20:56:50 +0100

    feat(shell): add fish shell completions setup

---
+++ fish/conf.d/completions.fish
@@ -0,0 +1,18 @@
+set -l completions_dir "$HOME/.config/fish/completions"
+if not test -d "$completions_dir"
+    mkdir -p "$completions_dir"
+end
+
+if command -q ruff
+    set cache_file "$completions_dir/ruff.fish"
+    if not test -f "$cache_file"
+        ruff generate-shell-completion fish >"$cache_file"
+    end
+end
+
+if command -q poetry
+    set cache_file "$completions_dir/poetry.fish"
+    if not test -f "$cache_file"
+        poetry completions fish >"$cache_file"
+    end
+end
