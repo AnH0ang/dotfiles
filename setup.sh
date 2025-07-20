c740c7c8dc181b381226d8adb2ae35dae9bd1b3e
An Hoang <anhoang31415@gmail.com>
2025-07-20 11:43:14 +0200

    feat(vscode): add mcp configuration file and update settings to remove inline mcp settings

--- setup.sh
+++ setup.sh
@@ -30,6 +30,8 @@ rm -f "${HOME}"/Library/'Application Support'/Code/User/settings.json
 ln -sf "${HOME}"/.config/vscode/settings.jsonc "${HOME}"/Library/'Application Support'/Code/User/settings.json
 rm -f "${HOME}"/Library/'Application Support'/Code/User/keybindings.json
 ln -sf "${HOME}"/.config/vscode/keybindings.jsonc "${HOME}"/Library/'Application Support'/Code/User/keybindings.json
+rm -f "${HOME}"/Library/'Application Support'/Code/User/mcp.json
+ln -sf "${HOME}"/.config/vscode/mcp.jsonc "${HOME}"/Library/'Application Support'/Code/User/mcp.json
 
 # zsh
 echo "Configure zsh..."
