979249b30a7b61fff28bab14d48407c93d7c36b3
An Hoang <anhoang31415@gmail.com>
2026-02-18 21:53:46 +0100

    feat(config): add Atuin, Zoxide, Fnox, and Mise initialization to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -10,31 +10,6 @@ if status is-interactive
     set fish_cursor_replace_one underscore
     set fish_cursor_visual block
 
-    if command -q zoxide
-        zoxide init fish | source
-    end
-
-    if command -q atuin
-        atuin init fish --disable-up-arrow | source
-    end
-
-    if command -q fnox
-        fnox activate fish | source
-    end
-
-    if command -q mise
-        mise activate fish | source
-    end
-
-    if command -q starship
-        starship init fish | source
-    end
-
-    # See https://github.com/microsoft/vscode/issues/208465
-    if test "$TERM_PROGRAM" = vscode
-        . (code --locate-shell-integration-path fish)
-    end
-
     # vi mode
     fish_vi_key_bindings
 
