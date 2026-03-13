26f17250b4402fd5e3cc586b0ae0f8df00f2d799
An Hoang <anhoang31415@gmail.com>
2026-03-13 12:42:57 +0100

    refactor(fish): comment out fish_prompt function for simplification

--- fish/config.fish
+++ fish/config.fish
@@ -36,9 +36,9 @@ if status is-interactive
     bind -M insert ctrl-e edit_command_buffer
 
     # Simplify prompt
-    function fish_prompt
-        string join '' -- (set_color green) (prompt_pwd) (set_color normal) ' > '
-    end
+    # function fish_prompt
+    #     string join '' -- (set_color green) (prompt_pwd) (set_color normal) ' > '
+    # end
 
     # Remove mode indicator
     function fish_mode_prompt
